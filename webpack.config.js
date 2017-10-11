398f4df9755468757bcd8e3ce87b13a92c0e9abd
Perchouli <jp.chenyang@gmail.com>
2017-10-11 23:25:46 +0800

    :arrow_up: Upgrade React and Babel, add ant design for MVP

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         use: [{
           loader: 'babel-loader',
           options: {
-            presets: ['react', 'es2015'],
+            presets: ['react', 'env'],
             plugins: ['transform-object-rest-spread']
           }
         }]
